0e7fc104d33af6979c41dcf9abcefae4a673cc21
Siddharth Telang <stelang@umd.edu>
2021-11-27 00:46:24 -0500

    Add new GTests

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,28 @@
 #include <gtest/gtest.h>
+#include "Sensor.hpp"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+TEST(readData, read) {
+  Reader *read = new Reader();
+  auto ret = read->readData(3);
+  ASSERT_EQ(ret, 5);
+}
+
+TEST(readData, invalid_data) {
+  Reader *read = new Reader();
+  auto ret = read->readData(-58);
+  ASSERT_EQ(ret, -1);
+}
+
+TEST(analog_sensor, readData) {
+  std::unique_ptr<Reader> reader(new Reader());
+  Sensor* s = new AnalogSensor(std::move(reader));
+  auto ret = s->read(10);
+  ASSERT_EQ(ret, 12);
+}
+
+TEST(analog_sensor, invalid_data) {
+  std::unique_ptr<Reader> reader(new Reader());
+  Sensor* s = new AnalogSensor(std::move(reader));
+  auto ret = s->read(-5);
+  ASSERT_EQ(ret, -1);
 }
