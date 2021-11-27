6c843f1a780021b312b2017c297eef3fc238a22f
Siddharth Telang <stelang@umd.edu>
2021-11-27 00:21:35 -0500

    update main class

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,9 @@
 /* Copyright Siddharth [2021] */
 #include <iostream>
-#include <AnalogSensor.hpp>
+#include <Sensor.hpp>
 
 int main() {
-    AnalogSensor lightSensor(5);
-    std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator = true;
-    if (terminator) {
-        std::cout << "DONE" << std::endl;
-    }
-    return 0;
+    std::unique_ptr<Reader> reader(new Reader());
+    Sensor* s = new AnalogSensor(std::move(reader));
+    std::cout << "read data = " << s->read(10) << std::endl;
 }
